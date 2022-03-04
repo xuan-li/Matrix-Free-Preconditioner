f7ccc4e337bf49a713e8618f2bdb8746654d12b1
Xuan Li <xuan.shayne.li@gmail.com>
2022-03-04 04:10:31 -0800

    test script

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@ int main(int argc, char** argv)
 {
     Eigen::SparseMatrix<double, Eigen::RowMajor> A; // row major to use eigen build-in parallelization
     Eigen::VectorXd rhs;
-    Eigen::read_binary_sparse(DATA_DIR"system_matrix_random.bin", A);
-    Eigen::read_binary(DATA_DIR"rhs_random.bin", rhs);
+    Eigen::read_binary_sparse(DATA_DIR"system_matrix_random_F.bin", A);
+    Eigen::read_binary(DATA_DIR"rhs_random_F.bin", rhs);
     LMPPreconditionedOracle oracle;
-    oracle.initialize(A);
+    oracle.initialize(A, 50);
     Minres<double, Oracle, Eigen::VectorXd> cg(100000);
     cg.setRelativeTolerance(1e-7);
     Eigen::VectorXd x = Eigen::VectorXd::Zero(rhs.size());
+    rhs = oracle.permute(rhs);
     cg.solve(oracle, x, rhs);
+    x = oracle.inverse_permute(x);
     return 0;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -19,7 +19,7 @@ public:
     // y_out = M * x_in
     void perform_op(const double *x_in, double *y_out) const
     {
-        Eigen::Map<const Eigen::VectorXd> v_in(x_in, rows(), rows());
+        Eigen::Map<const Eigen::VectorXd> v_in(x_in, rows());
         Eigen::VectorXd temp(rows());
         A.multiply(v_in, temp);
         Eigen::VectorXd result(rows());
@@ -33,38 +33,30 @@ public:
 
 int main(int argc, char** argv)
 {
+    // Eigen::VectorXd vec(5);
+    // vec << 1,2,3,4,5 ;
+    // SpMat A = Eigen::MatrixXd(vec.asDiagonal()).sparseView();
     SpMat A; // row major to use eigen build-in parallelization
-    Eigen::loadMarket(A, DATA_DIR"494_bus.mtx");
+    Eigen::loadMarket(A, DATA_DIR"bcsstk01.mtx", true);
+    // Eigen::SparseMatrix<double, Eigen::RowMajor> A; // row major to use eigen build-in parallelization
+    // Eigen::read_binary_sparse(DATA_DIR"system_matrix_random_F.bin", A);
     LMPPreconditionedOracle oracle;
-    oracle.initialize(A);
+    oracle.initialize(A, 10);
     ProblemWrapper op(oracle);
     op.precond = true;
-    GenEigsSolver<ProblemWrapper> eigs(op, 20, 40);
+    GenEigsSolver<ProblemWrapper> eigs(op, 1, 5);
     eigs.init();
     eigs.compute(SortRule::LargestMagn);
     if (eigs.info() == CompInfo::Successful)
     {
-        Eigen::VectorXd evalues = eigs.eigenvalues().real();
-        std::cout << "Precond largest Eigenvalues found:\n" << evalues.transpose() << std::endl;
-    }
-    eigs.compute(SortRule::SmallestMagn);
-    if (eigs.info() == CompInfo::Successful)
-    {
-        Eigen::VectorXd evalues = eigs.eigenvalues().real();
-        std::cout << "Precond smallest Eigenvalues found:\n" << evalues.transpose() << std::endl;
+        std::cout << "Precond largest Eigenvalues found:\n" << eigs.eigenvalues().transpose().real() << std::endl;
     }
     op.precond = false;
+    eigs.init();
     eigs.compute(SortRule::LargestMagn);
     if (eigs.info() == CompInfo::Successful)
     {
         Eigen::VectorXd evalues = eigs.eigenvalues().real();
         std::cout << "Nonprecond largest Eigenvalues found:\n" << evalues.transpose() << std::endl;
     }
-    eigs.compute(SortRule::SmallestMagn);
-    if (eigs.info() == CompInfo::Successful)
-    {
-        Eigen::VectorXd evalues = eigs.eigenvalues().real();
-        std::cout << "Nonprecond smallest Eigenvalues found:\n" << evalues.transpose() << std::endl;
-    }
-
 }
