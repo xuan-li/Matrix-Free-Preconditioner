e723db01f74a5868b6cbce60cc43897981870fd9
Xuan Li <xuan.shayne.li@gmail.com>
2022-03-04 02:16:25 -0800

    !! Eigen solves PAPT = LDLT..

--- src/PreconditionedProblem/LMPPrecondition.h
+++ src/PreconditionedProblem/LMPPrecondition.h
@@ -40,13 +40,13 @@ public:
         int i = 0;
         for (int j = 0; j < n; ++j) {
             if (this->diagonal_entries[j] > pivot) {
-                perm[i++] = j;
+                perm[j] = i++;
                 flag(j) = 1;
             }
         }
         for (int j = 0; j < n; ++j) {
             if (flag(j) == 0)
-                perm[i++] = j;
+                perm[j] = i++;
         }
 
         Matd H11, H21;
@@ -63,16 +63,29 @@ public:
             H21.col(i) = hi.bottomRows(n - k);
         }
 
+        // !!!!!!!! Eigen solves PAPT = LDLT.... !!!!!!!
         Eigen::LDLT<Matd> ldlt;
         ldlt.compute(H11);
+        H11 = ldlt.matrixLDLT();
         d1 = ldlt.vectorD();
         L11 = ldlt.matrixL();
         L11T = ldlt.matrixU();
+        auto perm2 = Eigen::PermutationMatrix<Eigen::Dynamic>(ldlt.transpositionsP()).indices();
+        for (int i = 0; i < n; ++ i) {
+            if (perm(i) < k)
+                perm(i) = perm2(perm(i));
+        }
+        // permute H21's cols
+        Matd new_H21;
+        for (int i = 0; i < k; ++i) {
+            new_H21.col(perm2(i)) = H21.col(i);
+        }
+        H21 = new_H21;
         Matd D1 = d1.asDiagonal();
         Matd D1_inv = D1;
         for (int i = 0; i < D1_inv.rows(); ++i)
         {
-            D1_inv(i, i) = 1.0 / D1_inv(i, i);
+            D1_inv(i, i) = 1.0 / D1(i, i);
         }
 
         L21 = (H21 * L11T.triangularView<Eigen::Upper>().solve(D1_inv)).sparseView();
@@ -83,7 +96,7 @@ public:
             double sum = 0;
             for (Eigen::SparseMatrix<double, Eigen::RowMajor>::InnerIterator it(L21, i); it; ++it)
             {
-                sum += it.value() * it.value() * D1(it.col());
+                sum += it.value() * it.value() * D1(it.col(), it.col());
             }
             d2(i) = diag[i + k] - sum;
         }
@@ -91,9 +104,10 @@ public:
 
     void precondition(const Vec& v, Vec& Pv) const override {
         // Pv = v;
+        // Vec diag = diagonal();
         // for (int i = 0; i < this->diagonal_entries.size(); ++i) {
-        //     if (abs(this->diagonal_entries[i]) > 0)
-        //         Pv[i] /= this->diagonal_entries[i];
+        //     if (abs(diag[i]) > 0)
+        //         Pv[i] /= diag[i];
         // }
 
         // [L11T  L21T]^(-1) [D1   ]^(-1) [L11   ]^(-1) 
@@ -112,22 +126,22 @@ public:
             y(i) /= d2(i);
         }
         x = L11T.triangularView<Eigen::Upper>().solve(x - L21.transpose() * y);
-        Pv.segment(0, k) += x;
-        Pv.segment(k, n - k) += y;
+        Pv.segment(0, k) = x;
+        Pv.segment(k, n - k) = y;
     }
 
     Vec permute(const Vec& v_in) const
     {
         Vec v_out = v_in;
         for (int i = 0; i < n; ++i) 
-            v_out(i) = v_in(perm(i));
+            v_out(perm(i)) = v_in(i);
         return v_out;
     }
     Vec inverse_permute(const Vec& v_in) const
     {
         Vec v_out = v_in;
         for (int i = 0; i < n; ++i) 
-            v_out(perm(i)) = v_in(i);
+            v_out(i) = v_in(perm(i));
         return v_out;
     }
 };
