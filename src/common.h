c06f223e84a84d34f9ff0716ab3c686ba83a0215
Xuan Li <xuan.shayne.li@gmail.com>
2022-03-10 16:28:29 -0800

    a final demo

--- src/common.h
+++
@@ -1,3 +0,0 @@
-#include <Eigen/Eigen>
-
-using SpMat = Eigen::SparseMatrix<double, Eigen::RowMajor, int>;
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,17 @@
 int main(int argc, char** argv)
 {
     Eigen::SparseMatrix<double, Eigen::RowMajor> A; // row major to use eigen build-in parallelization
-    Eigen::VectorXd rhs;
     Eigen::read_binary_sparse(DATA_DIR"system_matrix_random_F.bin", A);
-    Eigen::read_binary(DATA_DIR"rhs_random_F.bin", rhs);
+    Eigen::VectorXd rhs = A * Eigen::VectorXd::Ones(A.rows());
+    std::cout << "Size of A :" << A.rows() << std::endl;
     LMPPreconditionedOracle oracle;
-    oracle.initialize(A, 50);
+    oracle.initialize(A, 20);
     Minres<double, Oracle, Eigen::VectorXd> cg(100000);
-    cg.setRelativeTolerance(1e-7);
+    cg.setRelativeTolerance(1e-5);
     Eigen::VectorXd x = Eigen::VectorXd::Zero(rhs.size());
     rhs = oracle.permute(rhs);
     cg.solve(oracle, x, rhs);
     x = oracle.inverse_permute(x);
+    std::cout << "Relative error to ground truth: " << Eigen::VectorXd((x.array() - 1)/x.size()).norm() << std::endl;
     return 0;
 }
